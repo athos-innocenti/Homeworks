0f95f7ba7b523813037918144475b0a4e7322c2b
Athos Innocenti <57680842+athos-innocenti@users.noreply.github.com>
2022-11-10 11:32:48 +0100

    Update homework_1.m

--- Dinamiche su Network/homework_1.m
+++ Dinamiche su Network/homework_1.m
@@ -16,7 +16,7 @@ G = graph(W);
 plot(G);
 
 % DEGREE CENTRALITY
-% degreeCentrality = centrality(G, 'degree', 'Importance', G.Edges.Weight);
+% Check: centrality(G, 'degree', 'Importance', G.Edges.Weight)
 degrees = zeros(length(W), 1);
 for i = 1:length(W)
     degrees(i) = sum(W(:, i));
@@ -24,21 +24,21 @@ end
 degreeCentrality = degrees;
 
 % EIGENVECTOR CENTRALITY
-% eigenvectorCentrality = centrality(G, 'eigenvector', 'Importance', G.Edges.Weight);
+% Check: centrality(G, 'eigenvector', 'Importance', G.Edges.Weight);
 [~, eigenvalues] = eig(W);
 lambdaW = max(eigenvalues, [], 'all');
-[eigenvectors, eigenvalues] = eig((1/lambdaW) * transpose(W));
+[eigenvectors, eigenvalues] = eig((1 / lambdaW) * transpose(W));
 eigenvalues = diag(eigenvalues);
 eigenvector = eigenvectors(:, eigenvalues == max(eigenvalues, [], 'all')); % prendo l'autovettore di autovalore 1
-eigenvectorCentrality = (1/sum(eigenvector) * eigenvector); % normalizzo rispetto alla somma
+eigenvectorCentrality = ((1 / sum(eigenvector)) * eigenvector); % normalizzo rispetto alla somma
 
 % INVARIANT DISTRIBUTION CENTRALITY
 D = diag(degrees);
 P = D^(-1) * W;
-[eigenvectorsP, eigenvaluesP] = eig(P);
+[eigenvectorsP, eigenvaluesP] = eig(transpose(P));
 eigenvaluesP = diag(eigenvaluesP);
-eigenvectorP = eigenvectorsP(:, eigenvaluesP == max(eigenvaluesP)); % prendo l'autovettore di autovalore 1
-invariantDistributionCentrality = (1/sum(eigenvectorP) * eigenvectorP); % normalizzo rispetto alla somma
+eigenvectorP = eigenvectorsP(:, eigenvaluesP == max(eigenvaluesP, [], 'all')); % prendo l'autovettore di autovalore 1
+invariantDistributionCentrality = ((1 / sum(eigenvectorP)) * eigenvectorP); % normalizzo rispetto alla somma
 
 % KATZ CENTRALITY
 beta = 0.15;
