e4ed021f47e0b3e8d89764e2ff5ea483afe43745
Athos Innocenti <57680842+athos-innocenti@users.noreply.github.com>
2022-11-11 20:04:22 +0100

    Update homework_1.m

--- Dinamiche su Network/homework_1.m
+++ Dinamiche su Network/homework_1.m
@@ -21,7 +21,7 @@ degrees = zeros(length(W), 1);
 for i = 1:length(W)
     degrees(i) = sum(W(:, i));
 end
-degreeCentrality = degrees;
+degreeCentrality = (1 / sum(degrees)) * degrees;
 
 % EIGENVECTOR CENTRALITY
 % Check: centrality(G, 'eigenvector', 'Importance', G.Edges.Weight);
@@ -30,7 +30,7 @@ lambdaW = max(eigenvalues, [], 'all');
 [eigenvectors, eigenvalues] = eig((1 / lambdaW) * transpose(W));
 eigenvalues = diag(eigenvalues);
 eigenvector = eigenvectors(:, eigenvalues == max(eigenvalues, [], 'all')); % eigenvector with lambda=1
-eigenvectorCentrality = ((1 / sum(eigenvector)) * eigenvector); % normalize: 1 / sum(eigenvec elements)
+eigenvectorCentrality = (1 / sum(eigenvector)) * eigenvector; % normalized 
 
 % INVARIANT DISTRIBUTION CENTRALITY
 D = diag(degrees);
@@ -38,9 +38,9 @@ P = D^(-1) * W;
 [eigenvectorsP, eigenvaluesP] = eig(transpose(P));
 eigenvaluesP = diag(eigenvaluesP);
 eigenvectorP = eigenvectorsP(:, eigenvaluesP == max(eigenvaluesP, [], 'all')); % eigenvector with lambda=1
-invariantDistributionCentrality = ((1 / sum(eigenvectorP)) * eigenvectorP); % normalize: 1 / sum(eigenvec elements)
+invariantDistributionCentrality = (1 / sum(eigenvectorP)) * eigenvectorP;
 
-% KATZ CENTRALITY
+% KATZ CENTRALITY (Iterative)
 % Check: (eye(14) - (((1 - beta) / lambdaW) * transpose(W)) )^(-1) * (beta * mu)
 beta = 0.15;
 mu = ones(length(W), 1);
@@ -52,6 +52,28 @@ while true
         break;
     end
 end
+katzCentrality = (1 / sum(katzCentrality)) * katzCentrality;
 
-% PAGERANK
-
+% PAGERANK (Distributed)
+% Check: centrality(G, 'pagerank', 'FollowProbability', 0.85)
+beta = 0.15;
+mu = 1;
+pageRank = zeros(length(W), 1);
+degrees = zeros(length(W), 1);
+for i = 1:length(W)
+    degrees(i) = sum(W(:, i));
+end
+while true
+    oldPageRank = pageRank;
+    for j = 1:length(W)
+        sigma = 0;
+        for k = 1:length(W)
+            sigma = sigma + (((1/degrees(k, 1)) * W(k, j)) * pageRank(k, 1));
+        end
+        pageRank(j, 1) = ((1 - beta) * sigma) + (beta * mu);
+    end
+    if pageRank == oldPageRank
+        break;
+    end
+end
+pageRank = (1 / sum(pageRank)) * pageRank;
